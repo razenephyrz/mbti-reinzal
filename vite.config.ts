0014295ad9ffc09fd0bbad08cd06051d788ffb9a
razenephyrz <reinzalkinas@gmail.com>
2025-11-18 21:04:36 +0700

    minor - rollup

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,23 @@ import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import devtoolsJson from 'vite-plugin-devtools-json';
 import { defineConfig } from 'vite';
+import wasm from '@rollup/plugin-wasm';
 
 export default defineConfig({
-	plugins: [sveltekit(), devtoolsJson(), tailwindcss()],
-	ssr: {
-		noExternal: ['tsparticles', '@tsparticles/engine', '@tsparticles/slim', '@tsparticles/svelte']
-	}
+  plugins: [
+    sveltekit(),
+    devtoolsJson(),
+    tailwindcss(),
+    wasm() // ← plugin valid
+  ],
+
+  ssr: {
+    noExternal: [
+      'tsparticles',
+      '@tsparticles/engine',
+      '@tsparticles/slim',
+      '@tsparticles/svelte'
+    ]
+  }
 });
+
